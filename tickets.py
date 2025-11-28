6f28a2858a8df4107489274421116e48dbfda328
lambertmukudzei <lambertmukudzei@gmail.com>
2025-11-28 16:20:56 +0400

    updated tickets.py

--- tickets.py
+++ tickets.py
@@ -2,8 +2,8 @@ import pandas as pd
 from app.data.db import connect_database
 
 def get_all_tickets():
-    """I must Get all tickets."""
+    """Get all IT tickets."""
     conn = connect_database()
-    df = pd.read_sql_query("SELECT * FROM it_tickets ORDER BY id DESC", conn)
+    df = pd.read_sql_query("SELECT * FROM it_tickets", conn)
     conn.close()
     return df
