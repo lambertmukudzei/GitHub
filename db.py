b32956c26742b7c4deda49c62d541a59511268bb
lambertmukudzei <lambertmukudzei@gmail.com>
2025-11-28 16:23:13 +0400

    updated db.py

--- db.py
+++ db.py
@@ -4,5 +4,8 @@ from pathlib import Path
 DB_PATH = Path("DATA") / "intelligence_platform.db"
 
 def connect_database(db_path=DB_PATH):
-    """this Connects to SQLite database."""
+    """
+    Connect to the SQLite database.
+    Creates the database file if it doesn't exist.
+    """
     return sqlite3.connect(str(db_path))
