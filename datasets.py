cfc0592b6e15be3530f0ca4cb9fc0405ce04df87
lambertmukudzei <lambertmukudzei@gmail.com>
2025-11-28 16:23:46 +0400

    updated datasets.py

--- datasets.py
+++ datasets.py
@@ -2,8 +2,8 @@ import pandas as pd
 from app.data.db import connect_database
 
 def get_all_datasets():
-    """I must now Get all datasets."""
+    """Get all datasets."""
     conn = connect_database()
-    df = pd.read_sql_query("SELECT * FROM datasets_metadata ORDER BY id DESC", conn)
+    df = pd.read_sql_query("SELECT * FROM datasets_metadata", conn)
     conn.close()
     return df
